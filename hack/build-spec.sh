58fc346e851059106eba075395c0a19e9a82e3d0
Jesús García Crespo <jesus@sevein.com>
2019-07-04 22:12:28 -0700

    Migrate project to modules + GoReleaser
    - Add .goreleaser.yml with GoReleaser config for automated releases.
    - Fix name of LICENSE file, add CONTRIBUTING.md.
    - Use multi-stage build in Dockerfile.
    - Migrate to modules:
      - Add go.{mod,sum}.
      - Remove Gopkg.{lock/toml}.
      - Remove vendor/. If you are concerned about speed, use GO_PROXY.
      - Track tool dependencies using tools.go.
      - Update Makefile and hack/* scripts.

--- hack/build-spec.sh
+++ hack/build-spec.sh
@@ -6,12 +6,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if ! which go-bindata > /dev/null 2>&1 ; then
-	echo >&2 "Cannot find go-bindata. Install with \"go get -u github.com/jteeuwen/go-bindata/...\""
-	echo >&2 "Aborting.";
-	exit 1;
-fi
-
 readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly __root="$(cd "$(dirname "${__dir}")" && pwd)"
 readonly __gopath="$(cd "$(dirname "${__root}/../../../")" && pwd)"
@@ -19,7 +13,7 @@ readonly __gopath="$(cd "$(dirname "${__root}/../../../")" && pwd)"
 echo "Compiling..."
 cd ${__root}
 
-go-bindata \
+io-bindata \
 	-o "./broker/message/specdata/specdata.go" \
 	-nometadata \
 	-pkg "specdata" \
--- hack/coverage.sh
+++ hack/coverage.sh
@@ -10,21 +10,21 @@ coverprofile=$(mktemp /tmp/coverage.XXXXXXXXXX)
 hash goveralls 2>/dev/null || go get github.com/mattn/goveralls
 
 generate_cover_data() {
-  go test -coverprofile="${coverprofile}" -covermode="${covermode}" $(go list ./... | grep -v publisher/pb)
+    go test -coverprofile="${coverprofile}" -covermode="${covermode}" $(go list ./...)
 }
 
 push_to_coveralls() {
-  goveralls -coverprofile="${coverprofile}" -service=travis-ci
+    goveralls -coverprofile="${coverprofile}" -service=travis-ci
 }
 
 generate_cover_data
 go tool cover -func "${coverprofile}"
 
 case "${1-}" in
-  --html)
-    go tool cover -html "${coverprofile}"
-    ;;
-  --coveralls)
-    push_to_coveralls
-    ;;
+    --html)
+        go tool cover -html "${coverprofile}"
+        ;;
+    --coveralls)
+        push_to_coveralls
+        ;;
 esac
