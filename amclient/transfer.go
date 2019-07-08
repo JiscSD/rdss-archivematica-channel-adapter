dea83823d09e015a92c9337c8575c3aa67ec9750
Jesús García Crespo <jesus@sevein.com>
2019-07-07 20:26:17 -0700

    amclient: support status endpoints

--- amclient/transfer.go
+++ amclient/transfer.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+
+	"github.com/twinj/uuid"
 )
 
 const transferBasePath = "api/transfer"
@@ -14,6 +16,7 @@ type TransferService interface {
 	Start(context.Context, *TransferStartRequest) (*TransferStartResponse, *Response, error)
 	Approve(context.Context, *TransferApproveRequest) (*TransferApproveResponse, *Response, error)
 	Unapproved(context.Context, *TransferUnapprovedRequest) (*TransferUnapprovedResponse, *Response, error)
+	Status(context.Context, string) (*TransferStatusResponse, *Response, error)
 }
 
 // TransferServiceOp handles communication with the Tranfer related methods of
@@ -107,6 +110,55 @@ func (s *TransferServiceOp) Unapproved(ctx context.Context, r *TransferUnapprove
 	}
 
 	payload := &TransferUnapprovedResponse{}
+	resp, err := s.client.Do(ctx, req, payload)
+
+	return payload, resp, err
+}
+
+type TransferStatusResponse struct {
+	ID           string `json:"uuid"`
+	Status       string `json:"status"`
+	Name         string `json:"name"`
+	SIPID        string `json:"sip_uuid"`
+	Microservice string `json:"microservice"`
+	Directory    string `json:"directory"`
+	Path         string `json:"path"`
+	Message      string `json:"message"`
+	Type         string `json:"type"`
+}
+
+func (t TransferStatusResponse) SIP() (string, bool) {
+	// From the API Docs:
+	//
+	// > Note: for consumers of this endpoint, it is possible for Archivematica
+	// > to return a status of COMPLETE without a sip_uuid. Consumers looking to
+	// > use the UUID of the AIP that will be created following Ingest should
+	// > therefore test for both a status of COMPLETE and the existence of
+	// > sip_uuid that does not also equal BACKLOG to ensure that they retrieve
+	// > it. This might mean an additional call to the status endpoint while this
+	// > data becomes available.
+	//
+	if t.Status != "COMPLETE" {
+		return "", false
+	}
+	if t.SIPID == "" || t.SIPID == "BACKLOG" {
+		return "", false
+	}
+	if _, err := uuid.Parse(t.SIPID); err != nil {
+		return "", false
+	}
+	return t.SIPID, true
+}
+
+func (s *TransferServiceOp) Status(ctx context.Context, ID string) (*TransferStatusResponse, *Response, error) {
+	path := fmt.Sprintf("%s/status/%s", transferBasePath, ID)
+
+	req, err := s.client.NewRequest(ctx, "GET", path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	payload := &TransferStatusResponse{}
 	resp, err := s.client.Do(ctx, req, payload)
 
 	return payload, resp, err
