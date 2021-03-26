f3fbbfe0035e7f7028b3a2c0ab8171dcef43d1a3
Jesús García Crespo <jesus@sevein.com>
2021-03-26 09:15:59 +0100

    Rewrite test using testify

--- adapter/handlers_test.go
+++ adapter/handlers_test.go
@@ -5,182 +5,163 @@ import (
 	"testing"
 
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/JiscSD/rdss-archivematica-channel-adapter/amclient"
 	"github.com/JiscSD/rdss-archivematica-channel-adapter/broker/message"
 )
 
-var md = describeDataset
-
-type mdTest struct {
-	objectTitle         string
-	creatorType         message.PersonRoleEnum
-	creatorGivenName    string
-	creatorFamilyName   string
-	publisherType       message.PersonRoleEnum
-	publisherGivenName  string
-	publisherFamilyName string
-	identifierType      message.IdentifierTypeEnum
-	identifierValue     string
-	expected            []string
+var metadataGenerationTests = map[string]struct {
+	researchObject message.ResearchObject // Given this research object,
+	expected       []string               // CSV record that we expect, as a slice of strings.
+}{
+	"No family name": {
+		researchObject: message.ResearchObject{
+			ObjectTitle: "ObjectTitle1",
+			ObjectPersonRole: []message.PersonRole{
+				{
+					Role: message.PersonRoleEnum_dataCreator,
+					Person: message.Person{
+						PersonGivenNames:  "Kat",
+						PersonFamilyNames: "",
+					},
+				},
+				{
+					Role: message.PersonRoleEnum_publisher,
+					Person: message.Person{
+						PersonGivenNames:  "Joan",
+						PersonFamilyNames: "",
+					},
+				},
+			},
+			ObjectIdentifier: []message.Identifier{
+				{
+					IdentifierType:  message.IdentifierTypeEnum_DOI,
+					IdentifierValue: "10.5072/FK2/QAWS8O",
+				},
+			},
+		},
+		expected: []string{"objects/", "Kat", "10.5072/FK2/QAWS8O", "Joan", "ObjectTitle1", "artDesignItem"},
+	},
+	"No given name": {
+		researchObject: message.ResearchObject{
+			ObjectTitle: "ObjectTitle2",
+			ObjectPersonRole: []message.PersonRole{
+				{
+					Role: message.PersonRoleEnum_dataCreator,
+					Person: message.Person{
+						PersonGivenNames:  "",
+						PersonFamilyNames: "Winter",
+					},
+				},
+				{
+					Role: message.PersonRoleEnum_publisher,
+					Person: message.Person{
+						PersonGivenNames:  "",
+						PersonFamilyNames: "Watson",
+					},
+				},
+			},
+			ObjectIdentifier: []message.Identifier{
+				{
+					IdentifierType:  message.IdentifierTypeEnum_DOI,
+					IdentifierValue: "10.5072/FK2/QAWS81",
+				},
+			},
+		},
+		expected: []string{"objects/", "Winter", "10.5072/FK2/QAWS81", "Watson", "ObjectTitle2", "artDesignItem"},
+	},
+	"Both names": {
+		researchObject: message.ResearchObject{
+			ObjectTitle: "ObjectTitle3",
+			ObjectPersonRole: []message.PersonRole{
+				{
+					Role: message.PersonRoleEnum_dataCreator,
+					Person: message.Person{
+						PersonGivenNames:  "Kat",
+						PersonFamilyNames: "Winter",
+					},
+				},
+				{
+					Role: message.PersonRoleEnum_publisher,
+					Person: message.Person{
+						PersonGivenNames:  "Joan",
+						PersonFamilyNames: "Watson",
+					},
+				},
+			},
+			ObjectIdentifier: []message.Identifier{
+				{
+					IdentifierType:  message.IdentifierTypeEnum_DOI,
+					IdentifierValue: "10.5072/FK2/QAWS82",
+				},
+			},
+		},
+		expected: []string{"objects/", "Winter, Kat", "10.5072/FK2/QAWS82", "Watson, Joan", "ObjectTitle3", "artDesignItem"},
+	},
+	"No matching enums": {
+		researchObject: message.ResearchObject{
+			ObjectTitle: "ObjectTitle4",
+			ObjectPersonRole: []message.PersonRole{
+				{
+					Role: message.PersonRoleEnum_editor,
+					Person: message.Person{
+						PersonGivenNames:  "Kat",
+						PersonFamilyNames: "Winter",
+					},
+				},
+				{
+					Role: message.PersonRoleEnum_other,
+					Person: message.Person{
+						PersonGivenNames:  "Joan",
+						PersonFamilyNames: "Watson",
+					},
+				},
+			},
+			ObjectIdentifier: []message.Identifier{
+				{
+					IdentifierType:  message.IdentifierTypeEnum_DOI,
+					IdentifierValue: "10.5072/FK2/QAWS83",
+				},
+			},
+		},
+		expected: []string{"objects/", "10.5072/FK2/QAWS83", "ObjectTitle4", "artDesignItem"},
+	},
 }
 
-var mdSlice = []mdTest{
-	// No family name.
-	mdTest{
-		"ObjectTitle1",
-		message.PersonRoleEnum_dataCreator,
-		"Kat",
-		"",
-		message.PersonRoleEnum_publisher,
-		"Joan",
-		"",
-		message.IdentifierTypeEnum_DOI,
-		"10.5072/FK2/QAWS8O",
-		[]string{"objects/", "Kat", "10.5072/FK2/QAWS8O", "Joan", "ObjectTitle1", "artDesignItem"},
-	},
-	// No given name.
-	mdTest{
-		"ObjectTitle2",
-		message.PersonRoleEnum_dataCreator,
-		"",
-		"Winter",
-		message.PersonRoleEnum_publisher,
-		"",
-		"Watson",
-		message.IdentifierTypeEnum_DOI,
-		"10.5072/FK2/QAWS81",
-		[]string{"objects/", "Winter", "10.5072/FK2/QAWS81", "Watson", "ObjectTitle2", "artDesignItem"},
-	},
-	// Both names.
-	mdTest{
-		"ObjectTitle3",
-		message.PersonRoleEnum_dataCreator,
-		"Kat",
-		"Winter",
-		message.PersonRoleEnum_publisher,
-		"Joan",
-		"Watson",
-		message.IdentifierTypeEnum_DOI,
-		"10.5072/FK2/QAWS82",
-		[]string{"objects/", "Winter, Kat", "10.5072/FK2/QAWS82", "Watson, Joan", "ObjectTitle3", "artDesignItem"},
-	},
-	// No matching enums.
-	mdTest{
-		"ObjectTitle4",
-		message.PersonRoleEnum_editor,
-		"Kat",
-		"Winter",
-		message.PersonRoleEnum_other,
-		"Joan",
-		"Watson",
-		message.IdentifierTypeEnum_DOI,
-		"10.5072/FK2/QAWS83",
-		[]string{"objects/", "10.5072/FK2/QAWS83", "ObjectTitle4", "artDesignItem"},
-	},
-}
-
-// makeMDSet will create a metadata set from a test data object.
-func makeMDSet(value *mdTest, fs *afero.Afero) *amclient.MetadataSet {
-	transferSession := amclient.TransferSession{}
-	researchObject := message.ResearchObject{}
-
-	metadataSet := amclient.NewMetadataSet(fs)
-
-	transferSession.Metadata = metadataSet
-
-	researchObject.ObjectTitle = value.objectTitle
-
-	person := message.Person{}
-	person.PersonGivenNames = value.creatorGivenName
-	person.PersonFamilyNames = value.creatorFamilyName
-
-	personRole := message.PersonRole{}
-	personRole.Role = value.creatorType
-	personRole.Person = person
-
-	researchObject.ObjectPersonRole = append(researchObject.ObjectPersonRole, personRole)
-
-	person = message.Person{}
-	person.PersonGivenNames = value.publisherGivenName
-	person.PersonFamilyNames = value.publisherFamilyName
-
-	personRole = message.PersonRole{}
-	personRole.Role = value.publisherType
-	personRole.Person = person
-
-	researchObject.ObjectPersonRole = append(researchObject.ObjectPersonRole, personRole)
-
-	identifers := []message.Identifier{}
-
-	identifier := message.Identifier{}
-	identifier.IdentifierValue = value.identifierValue
-	identifier.IdentifierType = message.IdentifierTypeEnum_DOI
-
-	identifers = append(identifers, identifier)
-
-	researchObject.ObjectIdentifier = identifers
-
-	md(&transferSession, &researchObject)
-
-	return metadataSet
-}
-
-// TestMetadataGeneration inspects some of the internal functionality
-// of the channel adapter and ensures that from a given metadata set
-// the correct fields are mapped into an object which then returns an
-// expected CSV configuration for Archivematica. The test round-trips
-// the CSV generation and checks that when it is consumed the row data
-// is as is expected.
+// TestMetadataGeneration inspects some of the internal functionality of the
+// channel adapter and ensures that from a given metadata set the correct fields
+// are mapped into an object which then returns an expected CSV configuration
+// for Archivematica. The test round-trips the CSV generation and checks that
+// when it is consumed the row data is as is expected.
 func TestMetadataGeneration(t *testing.T) {
+	for name, tc := range metadataGenerationTests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
 
-	const mdFile string = "/metadata/metadata.csv"
-
-	for _, value := range mdSlice {
-
-		fs := afero.Afero{Fs: afero.NewBasePathFs(afero.NewMemMapFs(), "/")}
-
-		metadataSet := makeMDSet(&value, &fs)
-
-		metadataSet.Write()
-
-		csvFile, err := fs.Open(mdFile)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		csvObject := csv.NewReader(csvFile)
-
-		// Read the first line so we can move onto the row data.
-		_, err = csvObject.Read()
-		if err != nil {
-			t.Errorf("Cannot read CSV file")
-		}
-
-		// Read the second line, the row data.
-		record, err := csvObject.Read()
-		if err != nil {
-			t.Errorf("Cannot read CSV file")
-		}
-
-		if len(record) != len(value.expected) {
-			t.Errorf(
-				"CSV record not correct length: '%d', expected: '%d', %s",
-				len(record),
-				len(value.expected),
-				value.expected,
-			)
-			continue
-		}
-		for idx := range record {
-			if record[idx] != value.expected[idx] {
-				t.Errorf(
-					"Incorrect value in CSV record: %s %s",
-					record[idx],
-					value.expected[idx],
-				)
-				break
+			fs := afero.Afero{Fs: afero.NewBasePathFs(afero.NewMemMapFs(), "/")}
+			transferSession := amclient.TransferSession{
+				Metadata: amclient.NewMetadataSet(fs),
 			}
-		}
+
+			describeDataset(&transferSession, &tc.researchObject)
+			transferSession.Metadata.Write()
+
+			const mdFile string = "/metadata/metadata.csv"
+			csvFile, err := fs.Open(mdFile)
+			assert.NoError(t, err, "cannot open CSV file")
+
+			csvObject := csv.NewReader(csvFile)
+
+			// Read the first line so we can move onto the row data.
+			_, err = csvObject.Read()
+			assert.NoError(t, err, "cannot read CSV file")
+
+			// Read the second line, the row data.
+			record, err := csvObject.Read()
+			assert.NoError(t, err, "cannot read CSV file")
+			assert.Equal(t, len(record), len(tc.expected), "unexpected length in the CSV record")
+			assert.EqualValues(t, tc.expected, record, "incorrect value in CSV record")
+		})
 	}
 }
